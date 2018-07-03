b6ec6818b558e72620669dda4679b70a61166174
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2018-07-03 16:34:49 +0200

    refactor: ignore certain 4XX errors on sentry

--- src/memote_webservice/settings.py
+++ src/memote_webservice/settings.py
@@ -17,6 +17,8 @@
 
 import os
 
+import werkzeug.exceptions
+
 
 __all__ = ("Development", "Testing", "Production")
 
@@ -49,6 +51,15 @@ class Default:
         # Time after which a successful result will be removed.
         self.RESULT_TTL = [604800]  # 7 days
         self.SENTRY_DSN = os.environ.get('SENTRY_DSN')
+        self.SENTRY_CONFIG = {
+            'ignore_exceptions': [
+                werkzeug.exceptions.BadRequest,
+                werkzeug.exceptions.Unauthorized,
+                werkzeug.exceptions.Forbidden,
+                werkzeug.exceptions.NotFound,
+                werkzeug.exceptions.MethodNotAllowed,
+            ]
+        }
         self.LOGGING = {
             'version': 1,
             'disable_existing_loggers': False,
