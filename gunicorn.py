50c9c90284693ea76acc0ad0730f88baa23d8385
Ali Kaafarani <ali@kvikshaug.no>
2018-11-05 13:35:41 +0100

    chore: monkeypatch gevent as early as possible

--- gunicorn.py
+++ gunicorn.py
@@ -15,6 +15,15 @@
 
 """Configure the gunicorn server."""
 
+# Ensure gevent is monkeypatched before ssl is imported (gunicorn does this too
+# late). This is only necessary when `preload_app` is True. The gevent warning
+# is still printed, but testing shows that recursion errors do not occur (eg. on
+# use of `requests`) when monkey-patching here.
+# See also https://github.com/gevent/gevent/issues/1016 and
+# https://github.com/benoitc/gunicorn/issues/1566
+import gevent.monkey
+gevent.monkey.patch_all()
+
 import os
 
 _config = os.environ["ENVIRONMENT"]
