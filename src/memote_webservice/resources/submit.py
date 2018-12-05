27b4f46b87db5ba4f51088dac631d7abfad0fd3e
Ali Kaafarani <ali@kvikshaug.no>
2018-12-05 11:19:40 +0100

    feat: prepend original filename to dumped model

--- src/memote_webservice/resources/submit.py
+++ src/memote_webservice/resources/submit.py
@@ -22,6 +22,7 @@ from itertools import chain
 from uuid import uuid4
 
 import structlog
+import werkzeug
 from cobra.io import load_json_model, read_sbml_model
 from cobra.io.sbml3 import CobraSBMLError
 from flask import abort
@@ -92,12 +93,12 @@ class Submit(MethodResource):
         except (CobraSBMLError, ValueError) as err:
             msg = f"Failed to parse model: {str(err)}"
             LOGGER.exception(msg)
-            self._dump_model(file_storage.filename, content)
+            self._dump_model(filename, content)
             abort(400, msg)
         except Exception:
             # Unexpected exception. Don't handle it here, but do dump the
             # submitted model for debugging before re-raising.
-            self._dump_model(file_storage.filename, content)
+            self._dump_model(filename, content)
             raise
         finally:
             content.close()
@@ -122,7 +123,7 @@ class Submit(MethodResource):
 
     @staticmethod
     def _dump_model(filename, content):
-        uuid = str(uuid4())
-        LOGGER.warning(f"Dumping uploaded model '{filename}' to file '{uuid}'")
-        with open(uuid, 'wb') as f:
-            f.write(content.getvalue())
+        unqiue_filename = f"{str(uuid4())}_{werkzeug.secure_filename(filename)}"
+        LOGGER.warning(f"Dumping uploaded model to '{unqiue_filename}'")
+        with open(unqiue_filename, 'wb') as file_:
+            file_.write(content.getvalue())
